1ef378be3b913b82479ee68513110a242f665be5
shixianbudu <shixianbudu@users.noreply.github.com>
2026-08-25 10:37:53 +0800

    fix: HashRouter 修复 Pages 子路径白屏；SW 改网络优先

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
